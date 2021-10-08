3b34abd1960d8ee2cc3c663e3daf94647bb7766a
kngurley <kngurley25@gmail.com>
2021-10-08 14:09:07 -0500

    add for loop for questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 // DOM elements from HTML
 var startButtonEl = document.querySelector("#start-button");
-var homePageEl = document.querySelector(".home-page")
+var homePageEl = document.querySelector(".home-page");
 
 // variables to create code questions
-var questionEl = document.createElement("div");
+var questionPageEl = document.createElement("div");
+questionPageEl.className = "question-page";
+var questionEl = document.createElement("h2");
+questionEl.className = "question";
 var answerListEl = document.createElement("ul");
 var choiceEl = document.createElement("li");
 
@@ -12,28 +15,32 @@ var questionBank = [
     {question: "what is xxx",
     options: ["x", "y", "z", "u"],
     answer: 1},
-    {question: "what is xxx",
-    options: ["x", "y", "z", "u"],
+    {question: "what is yyy",
+    options: ["a", "y", "z", "u"],
     answer: 2},
-    {question: "what is xxx",
-    options: ["x", "y", "z", "u"],
+    {question: "what is zzz",
+    options: ["b", "y", "z", "u"],
     answer: 3},
-    {question: "what is xxx",
-    options: ["x", "y", "z", "u"],
+    {question: "what is uuu",
+    options: ["c", "y", "z", "u"],
     answer: 4}
 ]
 
-// function to loop through questions
-for (var i = 0; i <questionBank.length; i++) {
-    var question = questionBank[i].question;
-    var options = questionBank[i].options;
-    
-    questionEl.textContent = question;
-    answerListEl.textContent = options;
+// loop through questions
+var quizQuestion = function ()  {
+    for (var i = 0; i <questionBank.length; i++) {
+        var question = questionBank[i].question;
+        var options = questionBank[i].options;
 
-    document.body.appendChild(questionEl);
-    document.body.appendChild(answerListEl);
+        questionEl.textContent = question;
+        answerListEl.textContent = options;
+        
+        questionPageEl.appendChild(questionEl);
+        questionPageEl.appendChild(answerListEl);
+    }
 }
+
+quizQuestion();
 
 
 
