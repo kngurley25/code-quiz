17945656ec58de976bdb142cef952a79e84b80d4
kngurley <kngurley25@gmail.com>
2021-10-08 19:27:38 -0500

    added function for change of state

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,16 +10,29 @@ var answerOptionEl = document.querySelector(".answer-option");
 
 var answerButtonEl = document.querySelector("#answer-button");
 
+// hide initial question HTML elements before starting quiz
 var state = questionPageEl.getAttribute("data-state");
-if (state === "hidden") {
-    questionPageEl.style.display = "none";
-    console.log(state);
+    if (state === "hidden") {
+        questionPageEl.style.display = "none";
+    }
+
+// function to keep a question page hidden until data state changes
+var showHideQuestions = function () {
+    var state = questionPageEl.getAttribute("data-state");
+    if (state === "hidden") {
+        questionPageEl.style.display = "none";
+    }
+    else {
+        questionPageEl.style.display = "block";
+    }
 }
 
 // function to start quiz
 var startQuiz = function () {
     homePageEl.style.display = "none";
     quizTimer();
+    questionPageEl.setAttribute("data-state","visible");
+    showHideQuestions();
 }
 
 // quiz timer
@@ -37,7 +50,29 @@ function quizTimer () {
     }, 1000);
 }
 
+// questions array
+var questionArray = [
+    { question: "What is xxx",
+        answers: {a: "abc", b: "bdc", c: "cde", d: "def"},
+        correct: "a",
+    },
+    { question: "What is yyy",
+        answers: {a: "abc", b: "bdc", c: "cde", d: "def"},
+        correct: "b",
+    },
+    { question: "What is zzz",
+        answers: {a: "abc", b: "bdc", c: "cde", d: "def"},
+        correct: "c",
+    },
+    { question: "What is uuu",
+        answers: {a: "abc", b: "bdc", c: "cde", d: "def"},
+        correct: "d",
+    }
+];
 
+var quizQuestions = function () {
+
+}
 
 // event listener to hide opening home page and start quiz when start button is clicked
 startButtonEl.addEventListener("click", startQuiz);
